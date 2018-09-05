f2edac5faef2771600773b12337a053ae9fd4a60
schoenhk <schoenhk@rose-hulman.edu>
2018-09-05 11:16:34 -0400

    Session 3

--- src/m4_calling_functions_returning_values.py
+++ src/m4_calling_functions_returning_values.py
@@ -7,8 +7,8 @@ This module demonstrates and practices:
   -- UNIT TESTING.
 
 Authors: David Mutchler, Dave Fisher, Vibha Alangar, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Hunter Schoenborn.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import m4t_tester
 
@@ -38,7 +38,7 @@ def main():
 def run_test_sum_of_digits():
     """ Tests the  sum_of_digits   function. """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this TEST function, as follows:
+    # DONE: 2. Implement this TEST function, as follows:
     #
     #  Step 1:  This TEST function tests the  sum_of_digits  function.
     #    So read the doc-string of the  sum_of_digits  function
@@ -72,9 +72,21 @@ def run_test_sum_of_digits():
     # TO DO: 2 (continued).
     # Below this comment, add 3 more test cases of your own choosing.
     # ------------------------------------------------------------------
-# Test 2:
+#    Test 2:
     expected = 8
     answer = sum_of_digits(314)
+    print('Test 1 expected:', expected)
+    print('       actual:  ', answer)
+#
+#    Test 3:
+    expected = 19
+    answer = sum_of_digits(568)
+    print('Test 1 expected:', expected)
+    print('       actual:  ', answer)
+#
+#    Test 4:
+    expected = 16
+    answer = sum_of_digits(178)
     print('Test 1 expected:', expected)
     print('       actual:  ', answer)
 
